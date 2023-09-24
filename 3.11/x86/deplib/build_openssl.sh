f1bcba4304569afb32eec995e509340fc17d3b14
scc <zxc25077667@gmail.com>
2023-09-24 22:55:29 +0800

    fix: typo unterminated quoted compare

--- 3.11/x86/deplib/build_openssl.sh
+++ 3.11/x86/deplib/build_openssl.sh
@@ -6,7 +6,7 @@ export OPENSSL_VERSION=1.1.1w
 wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 tar -xvf openssl-${OPENSSL_VERSION}.tar.gz && cd openssl-${OPENSSL_VERSION}
 
-if [ "$IS_32BIT"" ]; then
+if [ "$IS_32BIT" = "0" ]; then
     ./config --prefix=/opt/shared_libraries
     make -j $(nproc) && make install
 elif [ "$IS_32BIT" = "1" ]; then
