70bb4857cb07d2518185ad1532592675cce920d6
scc <scc@teamt5.org>
2024-04-29 17:47:51 +0800

    chore: Upgrade bundled Expat to 2.6.0
    Update the bundled Expat library to version 2.6.0 for the fix
    related to CVE-2023-52425.

--- 3.11/x86_64/deplib/build_expat.sh
+++ 3.11/x86_64/deplib/build_expat.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# https://github.com/libexpat/libexpat/releases/download/R_2_5_0/expat-2.5.0.tar.gz
+# https://github.com/libexpat/libexpat/releases/download/R_2_6_0/expat-2.6.0.tar.gz
+# Please upgrade bundled Expat to 2.6.0 (e.g. for the fix to CVE-2023-52425)
+# https://github.com/python/cpython/issues/115399
 set -e
-export EXPAT_VERSION=2.5.0
-export EXPAT_LITERIAL_VERSION=2_5_0
+export EXPAT_VERSION=2.6.0
+export EXPAT_LITERIAL_VERSION=2_6_0
 
 wget https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_LITERIAL_VERSION}/expat-${EXPAT_VERSION}.tar.gz
 
